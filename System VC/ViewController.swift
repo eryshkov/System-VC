b0acb5b8dad17f90600b1c190b3dfaee9bd3aea6
eryshkov <wendor265>
2018-10-15 22:01:09 +0300

    init commit

--- System VC/ViewController.swift
+++ System VC/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //MARK: - IBActions
+    
+    @IBAction func shareButtonPressed(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        //for big screen sizes
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    @IBAction func safariButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func emailButtonPressed(_ sender: UIButton) {
+        
+    }
 }
 
