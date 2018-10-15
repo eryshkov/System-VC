40200c4f9a1460b50f0895e8ce4e22b70e89ee96
eryshkov <wendor265>
2018-10-15 21:34:18 +0300

    Initial Commit

---
+++ System VC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System VC
+//
+//  Created by Evgeniy Ryshkov on 15/10/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
